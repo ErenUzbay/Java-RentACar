7e93ead826dd408a0ae8da9b954e7cdbe5c41a5b
Eren Uzbay <erenuzbay@gmail.com>
2022-11-02 23:02:36 +0300

    Java Yazılım Geliştirici Yetiştirme Kampı 5: Spring Boot 2
    Java Yazılım Geliştirici Yetiştirme Kampı 5: Spring Boot 2

---
+++ src/main/java/kodlama/io/rentACar/business/responses/GetAllBrandsResponse.java
@@ -0,0 +1,13 @@
+package kodlama.io.rentACar.business.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetAllBrandsResponse { // Api den UI giden veri
+	private int id;
+	private String name;
+}
--- src/main/java/kodlama/io/rentACar/webAPI/controllers/BrandsController.java
+++ src/main/java/kodlama/io/rentACar/webAPI/controllers/BrandsController.java
@@ -4,13 +4,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlama.io.rentACar.business.abstracts.BrandService;
-import kodlama.io.rentACar.entities.concretes.Brand;
+import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
 
-@RestController // Annotation
+@RestController 
 @RequestMapping("/api/brands")
 public class BrandsController {
     private BrandService brandService;
@@ -21,7 +24,12 @@ public class BrandsController {
     }
     
     @GetMapping("/getall")
-    public List<Brand> getAll(){
+    public List<GetAllBrandsResponse> getAll(){
         return brandService.getAll();
+    }
+    
+    @PostMapping("/add")
+    public void add(@RequestBody() CreateBrandRequest createBrandRequest) {
+    	brandService.Add(createBrandRequest);
     }
 }
